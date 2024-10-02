d93cd2ec86ef3a6e58dbefaf370bbf4eff84bc94
Ryan Bramwell CESM2022 <ryan.bramwell.2022@uni.strath.ac.uk>
2024-10-02 21:38:16 +0100

    setup

--- dist-newstyle/build/x86_64-linux/ghc-8.8.4/BurgerTime-0.1.0.0/x/BurgerTime/build/BurgerTime/autogen/Paths_BurgerTime.hs
+++ dist-newstyle/build/x86_64-linux/ghc-8.8.4/BurgerTime-0.1.0.0/x/BurgerTime/build/BurgerTime/autogen/Paths_BurgerTime.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE NoRebindableSyntax #-}
+#if __GLASGOW_HASKELL__ >= 810
+{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
+#endif
 {-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
 module Paths_BurgerTime (
     version,
     getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
     getDataFileName, getSysconfDir
   ) where
 
+
 import qualified Control.Exception as Exception
+import qualified Data.List as List
 import Data.Version (Version(..))
 import System.Environment (getEnv)
 import Prelude
+
 
 #if defined(VERSION_base)
 
@@ -27,8 +34,18 @@ catchIO = Exception.catch
 
 version :: Version
 version = Version [0,1,0,0] []
-bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 bindir     = "/home/ryanb/.cabal/bin"
 libdir     = "/home/ryanb/.cabal/lib/x86_64-linux-ghc-8.8.4/BurgerTime-0.1.0.0-inplace-BurgerTime"
 dynlibdir  = "/home/ryanb/.cabal/lib/x86_64-linux-ghc-8.8.4"
@@ -36,15 +53,25 @@ datadir    = "/home/ryanb/.cabal/share/x86_64-linux-ghc-8.8.4/BurgerTime-0.1.0.0
 libexecdir = "/home/ryanb/.cabal/libexec/x86_64-linux-ghc-8.8.4/BurgerTime-0.1.0.0"
 sysconfdir = "/home/ryanb/.cabal/etc"
 
-getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
-getBinDir = catchIO (getEnv "BurgerTime_bindir") (\_ -> return bindir)
-getLibDir = catchIO (getEnv "BurgerTime_libdir") (\_ -> return libdir)
-getDynLibDir = catchIO (getEnv "BurgerTime_dynlibdir") (\_ -> return dynlibdir)
-getDataDir = catchIO (getEnv "BurgerTime_datadir") (\_ -> return datadir)
+getBinDir     = catchIO (getEnv "BurgerTime_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "BurgerTime_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "BurgerTime_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "BurgerTime_datadir")    (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "BurgerTime_libexecdir") (\_ -> return libexecdir)
 getSysconfDir = catchIO (getEnv "BurgerTime_sysconfdir") (\_ -> return sysconfdir)
 
-getDataFileName :: FilePath -> IO FilePath
-getDataFileName name = do
-  dir <- getDataDir
-  return (dir ++ "/" ++ name)
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
---
+++ src/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE PatternSynonyms #-}
+
+module Main where
+
+import Raylib.Core (clearBackground, disableCursor)
+import Raylib.Core.Camera (updateCamera)
+import Raylib.Core.Models (drawCircle3D, drawCubeWiresV, drawLine3D)
+import Raylib.Core.Text (drawFPS)
+import Raylib.Types (Camera3D (Camera3D), CameraMode (CameraModeFirstPerson), CameraProjection (CameraPerspective), pattern Vector3)
+import Raylib.Util (drawing, mode3D, whileWindowOpen_, withWindow)
+import Raylib.Util.Colors (black, white)
+
+main :: IO ()
+main = do
+  withWindow
+    600
+    450
+    "raylib [core] example - first person camera"
+    60
+    ( \_ -> do
+        disableCursor
+
+        let camera = Camera3D (Vector3 0 0 0) (Vector3 2 0 1) (Vector3 0 1 0) 70 CameraPerspective
+
+        whileWindowOpen_
+          ( \c ->
+              drawing
+                ( do
+                    clearBackground black
+                    drawFPS 10 20
+
+                    mode3D
+                      c
+                      ( do
+                          drawCircle3D (Vector3 2 0 1) 2 (Vector3 0 0 0) 0 white
+                          drawLine3D (Vector3 3 (-1) 1) (Vector3 1 1 1) white
+                          drawLine3D (Vector3 4 2 2) (Vector3 1 (-1) 1) white
+                          drawCubeWiresV (Vector3 (-2) 0 0) (Vector3 1 1 1) white
+                      )
+                )
+                >> updateCamera c CameraModeFirstPerson
+          )
+          camera
+    )
